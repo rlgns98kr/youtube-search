12868f96c6b9eb6bd00f63cb1f7209354165083c
GiHoon <rlgns98kr@gmail.com>
2020-06-16 14:57:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Youtube Search</h1>
+    <SearchBar />
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
